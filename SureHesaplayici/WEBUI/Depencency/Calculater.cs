cbe566ac552dbeecb42668660e2aab3d605a3c48
Mert Burak Dervişoğlu <mertburakdervisoglu@gmail.com>
2023-01-20 16:20:02 +0300

    düzeltme

--- SureHesaplayici/WEBUI/Depencency/Calculater.cs
+++ SureHesaplayici/WEBUI/Depencency/Calculater.cs
@@ -2,13 +2,8 @@
 {
     public static class Calculater
     {
-    public static string CalculateApp(List<int> minutes, int perEightHour)
+        public static string CalculateApp(List<int> minutes, int perEightHour)
         {
-           // int perEightHour = 975;
-           // List<int> minutes = new List<int> {
-           //420,240,120,240,30,200,430,420,600,360
-           // };
-
             long summ = minutes.Sum(q => q);
 
             decimal ToplamSaat = Convert.ToDecimal((float)summ / (float)60);
