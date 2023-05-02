fb2648f0a7c1fdf7b008b1d9a06847ffeb5c9b10
Mert Burak Dervişoğlu <mertburakdervisoglu@gmail.com>
2023-05-02 19:41:06 +0300

    full push

--- SureHesaplayici/WEBUI/Controllers/HomeController.cs
+++ SureHesaplayici/WEBUI/Controllers/HomeController.cs
@@ -2,20 +2,55 @@
 using System.Diagnostics;
 using WEBUI.Depencency;
 using WEBUI.Models;
+using System.Diagnostics;
+using System;
+using Microsoft.Extensions.Hosting;
 
 namespace WEBUI.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _environment;
+        public HomeController(ILogger<HomeController> logger, Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
         {
             _logger = logger;
+            _environment = environment;
         }
 
         public IActionResult Index(WorkPerModel result)
         {
+            //var process = new Process
+            //{
+            //    StartInfo = new ProcessStartInfo
+            //    {
+            //        FileName = "docker",
+            //        Arguments = "build -t testing:0.1 .",
+            //        RedirectStandardOutput = true,
+            //        UseShellExecute = false,
+            //        CreateNoWindow = true,
+            //        WorkingDirectory = "C:\\Users\\MERSUS\\Documents\\GitHub\\WorkingTimeCalculator\\SureHesaplayici\\WEBUI\\wwwroot\\carrefoursa-codeandmorecms"
+            //    }
+            //};
+            //process.Start();
+            //string output = process.StandardOutput.ReadToEnd();
+            //process.WaitForExit();
+
+            //return Content(output);
+
+
+            //ProcessStartInfo _processStartInfo = new ProcessStartInfo();
+
+            //_processStartInfo.WorkingDirectory = "C:\\Users\\MERSUS\\Documents\\GitHub\\WorkingTimeCalculator\\SureHesaplayici\\WEBUI\\wwwroot\\carrefoursa-codeandmorecms\\CodeAndMoreNuget";
+            ////_processStartInfo.CreateNoWindow = true;
+            //Process myProcess = Process.Start(_processStartInfo);
+            //myProcess.StartInfo.FileName = "C:\\WINDOWS\\system32\\cmd.exe";
+            //myProcess.StartInfo.Arguments = "/c dir";
+            //myProcess.StartInfo.UseShellExecute = true;
+            //myProcess.Start();
+            //string output = myProcess.StandardOutput.ReadToEnd();
+            //Console.WriteLine(output);
+
             return View(result);
         }
 
